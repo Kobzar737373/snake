b39829809afd1c501f2537f73dbd19061451c6a4
kobzar737373 <kobzar737373@gmail.ru..com>
2020-03-29 17:56:29 +0400

    Добавьте файлы проекта.

---
+++ Snake/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(2, 3, '#');
+            p1.draw();
+
+            Point p2 = new Point(4, 6, '*');
+            p2.draw();
+
+            Console.ReadLine();
+        }
+    }
+}
